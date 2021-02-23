6ee4449a26f051b036be9d1312f0a01e0ae3ec0c
Ricardo <ricardorincon911@gmail.com>
2021-02-23 21:52:14 +0100

    Added red trail on mouse hover for each grid square

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const container = document.getElementById('container');
 
 for (i = 0; i < 256; i++) {
-
   let gridSquare = document.createElement('div');
   gridSquare.className = 'square'
   container.appendChild(gridSquare);
-
 }
+
+const squares = Array.from(document.getElementsByClassName('square'));
+squares.forEach(square => {
+  square.addEventListener('mouseover', changeColor);  
+});
+
+function changeColor(e) {
+  e.target.style.backgroundColor = 'rgba(255, 0, 0, 0.3)';  
+};
