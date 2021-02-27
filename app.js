0c4a0a0067bd78f54df8341a9660e812950be83a
Ricardo <ricardorincon911@gmail.com>
2021-02-27 17:09:41 +0100

    Added selected-button glow managing function

--- app.js
+++ app.js
@@ -1,36 +1,12 @@
 const container = document.getElementById('container');
 const clear = document.getElementById('clear');
 
-// Current color scheme, scheme options container
-let currentScheme = 'rainbow';
-const fakeBtn = document.getElementById('fake-button')
-const schemesContainer = document.getElementById('schemes-container');
-const schemeOptions = Array.from(document.getElementsByClassName('scheme'));
-schemeOptions.forEach(scheme => {
-  scheme.addEventListener('click', changeColorScheme);
-})
-console.log(schemeOptions);
-
-function changeColorScheme(e) {
-  let scheme = e.target;
-
-  if (scheme.id == 'monochrome') {
-    currentScheme = 'monochrome';   
-    schemeOptions[0].classList.remove('selected-btn');
-    schemeOptions[2].classList.remove('selected-btn');
-    fakeBtn.classList.add('selected-btn')
-  } else if (scheme.id == 'grayscale') {
-    currentScheme = 'grayscale';
-    schemeOptions[0].classList.remove('selected-btn');
-    fakeBtn.classList.remove('selected-btn');
-    scheme.classList.add('selected-btn')
-  } else {
-    currentScheme = 'rainbow';
-    schemeOptions[1].classList.remove('selected-btn');
-    schemeOptions[2].classList.remove('selected-btn');
-    scheme.classList.add('selected-btn')
-  }
-}
+// Default color mixer and value changes upon event
+const mixer = document.getElementById("monochrome");
+let mixerColor = mixer.value;
+mixer.addEventListener('change', () => {
+  mixerColor = mixer.value;
+});
 
 // Coloring mode, which changes from hover to click 'n' drag depending on button clicked
 let coloringMode = 'drag';
@@ -42,25 +18,76 @@ colorModes.forEach(mode => {
 function changeColorMode (e) {
   if (e.target.id == 'drag-button') {
     coloringMode = 'drag';
-    colorModes[1].classList.remove('selected-btn');
-    colorModes[0].classList.add('selected-btn');
+    manageGlow(colorModes, e.target.id);
     setArrayColorMode(coloringMode);
   } else if (e.target.id == 'hover-button') {
     coloringMode = 'hover';
-    colorModes[0].classList.remove('selected-btn');
-    colorModes[1].classList.add('selected-btn');
+    manageGlow(colorModes, e.target.id);
     setArrayColorMode(coloringMode);
   }
 }
 
-// Default color mixer and value changes upon event
-const mixer = document.getElementById("monochrome");
-let mixerColor = mixer.value;
-mixer.addEventListener('change', () => {
-  mixerColor = mixer.value;
+// Current color scheme, scheme options container
+let currentScheme = 'rainbow';
+const fakeBtn = document.getElementById('fake-button')
+const schemesContainer = document.getElementById('schemes-container');
+const schemeOptions = Array.from(document.getElementsByClassName('scheme'));
+schemeOptions.forEach(scheme => {
+  scheme.addEventListener('click', changeColorScheme);
 });
 
-// UI on document load
+function changeColorScheme(e) {
+  let scheme = e.target;
+
+  if (scheme.id == 'monochrome') {
+    currentScheme = 'monochrome';  
+    manageGlow(schemeOptions, scheme.id)
+  } else if (scheme.id == 'grayscale') {
+    currentScheme = 'grayscale';
+    manageGlow(schemeOptions, scheme.id)
+  } else {
+    currentScheme = 'rainbow';
+    manageGlow(schemeOptions, scheme.id)
+  }
+}
+
+// Adds and removes glow to current coloring mode and color scheme
+function manageGlow(array, id) {
+  for (i = 0; i < array.length; i++) {
+    if (array[i].id != id) {
+      array[i].classList.remove('selected-btn');
+    } else {
+      array[i].classList.add('selected-btn');
+    }
+
+    if (id == 'monochrome') {
+      fakeBtn.classList.add('selected-btn');
+    } else {
+      fakeBtn.classList.remove('selected-btn');
+    }
+  }
+}
+
+// Round the corners of the grid/squares
+function roundCorners(input) {
+  let squares = container.children;
+
+  if (input == 1) {
+    squares[0].style.borderRadius = '5rem';
+  } else {
+    let upperLeft = container.firstElementChild;
+    let upperRight = squares[input-1];
+    let lowerLeft = squares[squares.length-input];
+    let lowerRight = container.lastElementChild;
+    
+    upperLeft.style.borderRadius = '5rem 0px 0px 0px';
+    upperRight.style.borderRadius = '0px 5rem 0px 0px';
+    lowerLeft.style.borderRadius = '0px 0px 0px 5rem';
+    lowerRight.style.borderRadius = '0px 0px 5rem 0px';
+  }
+}
+
+// Document load UI
 defaultGrid();
 clear.addEventListener('change', resetGrid);
 
@@ -141,7 +168,7 @@ function setArrayColorMode(coloringMode) {
 
 }
 
-// Change color by hovering over square 
+// Change color by hovering over square (uses current color scheme)
 function colorByHovering(e) {
   
   if (currentScheme === 'rainbow') {
@@ -158,40 +185,18 @@ function colorByHovering(e) {
   }
 };
 
-// Change color if the event target was clicked and the button held down
-// Checks whether there are > 0 mouse buttons pressed when the cursor enters a new square
+// Change color if e.target clicked and the button held down (uses current color scheme)
 function colorByDragging(e) {
   if (e.buttons > 0) {  
     if (currentScheme === 'rainbow') {
       let red = Math.floor(Math.random() * 255);
       let green = Math.floor(Math.random() * 255);
       let blue = Math.floor(Math.random() * 255);
-      e.target.style.backgroundColor = `rgba(${red}, ${green}, ${blue}, 0.5)`; 
-  
+      e.target.style.backgroundColor = `rgba(${red}, ${green}, ${blue}, 0.5)`;   
     } else if (currentScheme === 'monochrome') {
-      e.target.style.backgroundColor = mixerColor;
-  
+      e.target.style.backgroundColor = mixerColor;  
     } else {
       e.target.style.backgroundColor = `hsl(0, 0%, 50%)`;
     }
   }
 };
-
-// Round the corners of the grid/squares
-function roundCorners(input) {
-  let squares = container.children;
-
-  if (input == 1) {
-    squares[0].style.borderRadius = '5rem';
-  } else {
-    let upperLeft = container.firstElementChild;
-    let upperRight = squares[input-1];
-    let lowerLeft = squares[squares.length-input];
-    let lowerRight = container.lastElementChild;
-    
-    upperLeft.style.borderRadius = '5rem 0px 0px 0px';
-    upperRight.style.borderRadius = '0px 5rem 0px 0px';
-    lowerLeft.style.borderRadius = '0px 0px 0px 5rem';
-    lowerRight.style.borderRadius = '0px 0px 5rem 0px';
-  }
-}
